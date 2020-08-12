d5f41b0f8a7138d8cbea7ec68c72f4358c8ad38d
Alex Basov <Basowf@yandex.ru>
2020-08-12 03:26:36 +0300

    lab6 demo-gui

---
+++ SiAOD6-2/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SiAOD6_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtBox = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(217, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(428, 310);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDoubleClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtBox);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 212);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // txtBox
+            // 
+            this.txtBox.DetectUrls = false;
+            this.txtBox.ImeMode = System.Windows.Forms.ImeMode.Close;
+            this.txtBox.Location = new System.Drawing.Point(7, 20);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(187, 150);
+            this.txtBox.TabIndex = 0;
+            this.txtBox.Text = "";
+            this.txtBox.WordWrap = false;
+            this.txtBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBox_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 335);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Johnson\'s algorithm on graph";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox txtBox;
+    }
+}
+
---
+++ SiAOD6-2/Form1.cs
@@ -0,0 +1,192 @@
+﻿// Thanks to "irreal" for their help with circles 
+// from https://stackoverflow.com/questions/29631529/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SiAOD6_2
+{
+    public partial class Form1 : Form
+    {
+        private List<MyCircle> myCircles = new List<MyCircle>();
+        private MyCircle activeCircle;
+        private const int RADIUS = 15;
+        private bool drawing = false;
+        private bool dragging = false;
+        private bool choosingStartEnd = false;
+        private int[] startEnd = { 0, 0 };
+        private readonly SolidBrush textColor = new SolidBrush(Color.White);
+        private Pen pen = new Pen(new SolidBrush(Color.Black), 4);
+        private Point dragOffset;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Node Visualisation & Controls
+        //Node class
+        public class MyCircle
+        {
+            public Point Point { get; set; }
+            public int Index { get; }
+            public SolidBrush myBrush { get; }
+            
+
+            public MyCircle(Point point, int index, Color color)
+            {
+                Point = point;
+                Index = index;
+                myBrush = new SolidBrush(color);
+            }
+
+            public void setColor(Color color)
+            {
+                myBrush.Color = color;
+            }
+        }
+        private MyCircle CheckIfCircleClicked(Point point)
+        {
+            //brush.Color = (Color.Red);
+            return myCircles.FirstOrDefault(
+                    circle =>
+                        Math.Abs(circle.Point.X - point.X) < RADIUS &&
+                        Math.Abs(circle.Point.Y - point.Y) < RADIUS);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                return;
+            }
+            else
+            {
+                drawing = false;
+                dragging = true;
+
+                activeCircle = CheckIfCircleClicked(e.Location);
+                if (activeCircle == null)
+                {
+
+                    activeCircle = new MyCircle(e.Location, myCircles.Count + 1, Color.Green);
+
+                    myCircles.Add(activeCircle);
+                    pictureBox1.Invalidate();
+
+                }
+                else
+                {
+                    dragging = true;
+                    dragOffset = new Point(activeCircle.Point.X - e.Location.X, activeCircle.Point.Y - e.Location.Y);
+                }
+            }
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                activeCircle.Point = new Point(e.Location.X + dragOffset.X, e.Location.Y + dragOffset.Y);
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button != MouseButtons.Left)
+                return;
+
+            if (activeCircle == null)
+                return;
+
+            if (dragging)
+            {
+                activeCircle.Point = new Point(e.Location.X + dragOffset.X, e.Location.Y + dragOffset.Y);
+            }
+            else if (drawing)
+            {
+                myCircles.Add(activeCircle);
+            }
+            dragging = false;
+            drawing = false;
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+
+            foreach (var circle in myCircles.Where(c => c != activeCircle))
+            {
+                g.DrawEllipse(pen, (circle.Point.X - RADIUS),
+                    (circle.Point.Y - RADIUS), (RADIUS * 2), (RADIUS * 2));
+                g.FillEllipse(circle.myBrush, (circle.Point.X - RADIUS), 
+                    (circle.Point.Y - RADIUS), (RADIUS * 2), (RADIUS * 2));
+                g.DrawString(circle.Index.ToString(), Font, textColor, 
+                    circle.Point.X - RADIUS/2, circle.Point.Y - RADIUS/2);
+
+            }
+
+            if (activeCircle != null)
+            {
+                g.DrawEllipse(pen, (activeCircle.Point.X - RADIUS),
+                    (activeCircle.Point.Y - RADIUS), (RADIUS * 2), (RADIUS * 2));
+                g.FillEllipse(activeCircle.myBrush, (activeCircle.Point.X - RADIUS),
+                    (activeCircle.Point.Y - RADIUS), (RADIUS * 2), (RADIUS * 2));
+                g.DrawString(activeCircle.Index.ToString(), Font, textColor, 
+                    activeCircle.Point.X - RADIUS/2, activeCircle.Point.Y - RADIUS/2);
+            }
+        }
+
+        #endregion
+
+        private void txtBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar) || e.KeyChar == 8 || e.KeyChar == 32)
+                e.Handled = false;
+            else e.Handled = true;
+        }
+
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            activeCircle = CheckIfCircleClicked(e.Location);
+            if(activeCircle != null)
+            {
+                if(startEnd[0] == 0 && startEnd[1] != activeCircle.Index)
+                {
+                    activeCircle.setColor(Color.Orange);
+                    startEnd[0] = activeCircle.Index;
+                }
+                else if(startEnd[0] == activeCircle.Index)
+                {
+                    activeCircle.setColor(Color.Green);
+                    startEnd[0] = 0;
+
+                }
+                if(startEnd[1] == 0 && startEnd[0] != activeCircle.Index)
+                {
+                    activeCircle.setColor(Color.OrangeRed);
+                    startEnd[1] = activeCircle.Index;
+                }
+                else if(startEnd[1] == activeCircle.Index)
+                {
+                    activeCircle.setColor(Color.Green);
+                    startEnd[1] = 0;
+                }
+                pictureBox1.Invalidate();
+            }
+        }
+    }
+}
